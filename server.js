88f85854f36d5d71c313c311d8432909c15bf21c
lbako801 <lbako98@gmail.com>
2022-12-29 23:11:46 -0700

    added initial boilerplate

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express(); 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json())
+
+const torqueRouter = require('./routes/torque')
+
+app.use('/torque', torqueRouter)
+
+app.listen(3000, () => console.log('Server has started!'));
